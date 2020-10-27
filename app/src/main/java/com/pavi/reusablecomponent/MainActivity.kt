07ad34aa1d2851fcfe00c99ae359c1140f5f62e3
Pavan <t.hemant1992@gmail.com>
2020-10-27 10:49:47 +0530

    adding gitResolveDependancy and widgetkit lib

--- app/src/main/java/com/pavi/reusablecomponent/MainActivity.kt
+++ app/src/main/java/com/pavi/reusablecomponent/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.pavi.reusablecomponent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pavi.reusablecomponentlibrary.DemoActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txtHelloHemant.setOnClickListener {
+            startActivity(Intent(this,DemoActivity::class.java))
+        }
     }
 }
